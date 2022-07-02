a439dfc0d06d44d97d84dd19c6793832393edc34
Bohdan2241 <bohdanyezhlov@gmail.com>
2022-07-02 08:59:11 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
+import { program } from 'commander';
 
-import program from '../index.js';
-
-program();
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .parse(process.argv);
